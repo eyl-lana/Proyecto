a4e229e12e5d5033068610edaa982f2c8315c768
eyl-lana <eyleen.sanchez.g@mail.pucv.cl>
2023-04-02 00:56:05 -0400

    acceso e registro

--- js/validacion.js
+++ js/validacion.js
@@ -1,10 +1,21 @@
- function validarFormulario(){
+ function validarFormularioRegistro(){
     var nombre = document.forms["registro"]["nombre"].value;
     var apellido = document.forms["registro"]["apellido"].value;
     var correo = document.forms["registro"]["correo"].value;
     var contrasena = document.forms["registro"]["contrasena"].value;
 
     if (nombre == "" || apellido == "" || correo == "" || contrasena == ""){
+        alert("Todos los campos son obligatorios");
+        return false;
+    }
+    return true;
+}
+
+function validarFormularioAcceso(){
+    var correo = document.forms["acceso"]["correo"].value;
+    var contrasena = document.forms["acceso"]["contrasena"].value;
+
+    if (correo == "" || contrasena == ""){
         alert("Todos los campos son obligatorios");
         return false;
     }
